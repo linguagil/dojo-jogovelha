ded14c9cd82c420cf66a5f3b16edcd228008f9b8
Cleverson Sacramento <cleverson.sacramento@gmail.com>
2011-09-16 09:29:47 -0300

    Initial Commit

---
+++ JogoVelha/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  JogoVelha
+//
+//  Created by Cleverson Sacramento on 16/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
